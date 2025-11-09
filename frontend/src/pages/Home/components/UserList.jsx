6f9c11fb07956374767339568f2b214db1a0c529
Yiğit İhtiyar <yigit.ihtiyar1@gmail.com>
2025-11-09 22:14:29 +0300

    update

---
+++ frontend/src/pages/Home/components/UserList.jsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+import { loadUsers } from "./api";
+
+export function UserList() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function getUsers() {
+      const response = await loadUsers();
+      setUsers(response.data);
+    }
+    getUsers();
+  }, []);
+  return (
+    <>
+      <div>User List</div>
+      {users.map((user) => {
+        return <div>{user.username}</div>;
+      })}
+    </>
+  );
+}
--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,4 +1,6 @@
+import { UserList } from "./components/UserList";
+
 export function HomePage(){
 
-    return <h1>Home Page</h1>
+    return <UserList />;
 }
