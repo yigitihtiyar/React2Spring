8e218e0a895f8594564d54c96469f3f0d0a0882c
Yiğit İhtiyar <yigit.ihtiyar1@gmail.com>
2026-02-11 18:24:16 +0300

    update

--- ws/src/main/java/com/example/ws/configuration/SecurityConfiguration.java
+++ ws/src/main/java/com/example/ws/configuration/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.example.ws.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -9,12 +10,17 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true) 
 public class SecurityConfiguration {
+
+    @Autowired
+    TokenFilter tokenFilter;
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((authentication) -> authentication
@@ -28,6 +34,8 @@ public class SecurityConfiguration {
         http.csrf(csrf -> csrf.disable());
 
         http.headers(headers -> headers.disable());
+
+        http.addFilterBefore(tokenFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
---
+++ ws/src/main/java/com/example/ws/configuration/TokenFilter.java
@@ -0,0 +1,43 @@
+package com.example.ws.configuration;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import com.example.ws.auth.token.TokenService;
+import com.example.ws.user.User;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class TokenFilter extends OncePerRequestFilter {
+
+    @Autowired
+    TokenService tokenService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader != null) {
+            User user = tokenService.verifyToken(authorizationHeader);
+            if (user != null) {
+                CurrentUser currentUser = new CurrentUser(user);
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(user,
+                        currentUser, currentUser.getAuthorities());
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+        filterChain.doFilter(request, response);
+
+    }
+
+}
